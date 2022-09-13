28939ee0f0860271130a96d71d583edda9516da7
Roy <forparty@gmail.com>
2022-09-13 21:05:08 +0300

    Final realise 1.0

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+from pprint import pprint
 
 from flask import Flask, request, jsonify
 from flask_restx import Api, Resource
@@ -6,11 +7,17 @@ from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
 
 app = Flask(__name__)
+app.json_ensure_ascii = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['RESTX_JSON'] = {'ensure_ascii': False}
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 db = SQLAlchemy(app)
 api = Api(app)
+
 movies_ns = api.namespace('movies')
+directors_ns = api.namespace('directors')
+genres_ns = api.namespace('genres')
 
 
 class Movie(db.Model):
@@ -47,6 +54,7 @@ class DirectorSchema(Schema):
 class GenreSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str()
+
 
 class MovieSchema(Schema):
     id = fields.Int(dump_only=True)
@@ -87,16 +95,19 @@ class MovieView(Resource):
             all_movie = Movie.query.all()
             if not all_movie:
                 return 'Error', 404
-            return movies_schema.dump(all_movie)
+            result = movies_schema.dump(all_movie)
+            return jsonify(result)
 
     def post(self):
         new_movie = request.json
-        if not new_movie:
+        if new_movie:
+            movie = Movie(**new_movie)
+            db.session.add(movie)
+            db.session.commit()
+            return 'Запись добавлена', 201
+        else:
             return 'Error', 404
-        movie = Movie(**new_movie)
-        db.session.add(movie)
-        db.session.commit()
-        return "putin loh", 200
+
 
 @movies_ns.route('/<int:uid>')
 class MovieView(Resource):
@@ -107,6 +118,7 @@ class MovieView(Resource):
             return jsonify(result['title'], result['description'])
         else:
             return 'Ничего не найдено', 404
+
     def put(self, uid):
         movie = Movie.query.get(uid)
         if not movie:
@@ -134,6 +146,78 @@ class MovieView(Resource):
         db.session.delete(movie)
         db.session.commit()
         return "", 204
+
+
+@directors_ns.route('/<int:uid>')
+class DirectorView(Resource):
+    def get(self, uid):
+        director = Director.query.get(uid)
+        if not director:
+            return 'Error', 404
+        result = director_schema.dump(director)
+        return result['name']
+
+    def put(self, uid):
+        director = Director.query.get(uid)
+        if not director:
+            return 'Error', 404
+        update_dir = request.json
+        [setattr(director, k, v) for k, v in update_dir.items()]
+        db.session.add(director)
+        db.session.commit()
+        return "", 204
+
+    def delete(self, uid):
+        director = Director.query.get(uid)
+        if not director:
+            return 'Error', 404
+        db.session.delete(director)
+        db.session.commit()
+        return "", 204
+
+
+@directors_ns.route('/')
+class DirectorView(Resource):
+    def get(self):
+        directors = Director.query.all()
+        if not directors:
+            return "Error", 404
+        return directors_schema.dump(directors)
+
+    def post(self):
+        new_dir = request.json
+        if new_dir:
+            director = Director(**new_dir)
+            db.session.add(director)
+            db.session.commit()
+        else:
+            return "Error", 404
+
+
+
+
+@genres_ns.route('/')
+class GenreView(Resource):
+    def get(self):
+        genres = Genre.query.all()
+        if genres:
+            return genres_schema.dump(genres)
+        else:
+            return "Error", 404
+
+@genres_ns.route('/<int:uid>')
+class GenreView(Resource):
+    def get(self, uid):
+        movie = db.session.query(Movie.title).filter(Movie.genre_id == uid)
+        genre = Genre.query.get(uid)
+        result_g = genre_schema.dump(genre)
+        all_ = []
+        all_.append(f"Жанр: {result_g['name']}")
+        all_.append("Фильмы:")
+        result_m = movies_schema.dump(movie)
+        [all_.append(m["title"]) for m in result_m]
+        return all_
+
 
 
 if __name__ == '__main__':
